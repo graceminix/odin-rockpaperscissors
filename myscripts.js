0b018700825444c7c4a6c6d5ecde69872928e1b6
Grace <graceminix@gmail.com>
2022-02-09 11:44:43 +0100

    Edited JS file and created index.  Current game stands at 5 rounds total, not 5 wins.

---
+++ myscripts.js
@@ -0,0 +1,72 @@
+var computerscore = 0;
+var playerscore = 0;
+
+function computerPlay() {
+    var computerrandom = Math.floor((Math.random() * 3) + 1)
+    if (computerrandom === 1) {
+        return("ROCK")
+    } else if (computerrandom === 2) {
+        return("PAPER")
+    } else {
+        return("SCISSORS")
+    }
+}
+
+function playRound(player, computerSelection) {
+    console.log(player)
+    console.log(computerSelection)
+    if (player === computerSelection) {
+        return("You have tied!")
+    } else if (player === "ROCK") {
+        if  (computerSelection === "PAPER") {
+            computerscore += 1
+            return("You lose! Paper beats rock!")
+        } if (computerSelection === "SCISSORS") {
+            playerscore += 1
+            return("You win!  Rock beats scissors!")
+        }
+    } else if (player === "PAPER") {
+        if (computerSelection === "ROCK") {
+            playerscore += 1
+            return("You win!  Paper beats rock!")
+        } if (computerSelection === "SCISSORS") {
+            computerscore += 1
+            return("You lose!  Scissors beats paper!")
+        }
+    } else {
+        if (computerSelection === "ROCK") {
+            computerscore += 1
+            return("You lose!  Rock beats scissors!")
+        } if (computerSelection === "PAPER") {
+            playerscore += 1
+            return("You win!  Scissors beats paper!")
+        }
+    } 
+}
+
+function playerChoice() {
+    var player = prompt("Rock, paper, or scissors?").toUpperCase()
+    if (player == "ROCK" || player == "PAPER" || player == "SCISSORS") {
+        return(player);       
+    } else {
+        console.log("You must choose only rock, paper, or scissors!")
+        return(playerChoice());
+    }
+}
+
+function game() {
+    for (i = 0; i < 5; i++) {
+        var computerSelection = computerPlay()
+        var player = playerChoice()
+        console.log(playRound(player, computerSelection))
+    }
+    if (playerscore > computerscore) {
+        console.log("You win!  Final score is " + playerscore + " to " + computerscore + "!")
+    } else if (computerscore > playerscore) {
+        console.log("You lose!  Final score is " + playerscore + " to " + computerscore + "!")
+    } else {
+        console.log("You tied! Final score is " + playerscore + " to " + computerscore + "!")
+    }
+}
+
+game()
