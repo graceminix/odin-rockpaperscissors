e803a058125596b6bc7e77fa8ee973d9ee7dea65
Grace <graceminix@gmail.com>
2022-02-10 17:24:05 +0100

    Updated readme, index, and myscripts.  Now playable in browser.

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # odin-rockpaperscissors
 
 A rock paper scissors game using JS where play rounds against a computer.  First to five wins! 
+
+Image of rock from: Image by sarajuggernaut from Pixabay
+
+Image of paper from: Photo by Elisabeth Ende from Pexels
+
+Image of scissors from: Photo by 165106 on Pixabay
--- myscripts.js
+++ myscripts.js
@@ -1,6 +1,23 @@
 var computerscore = 0;
 var playerscore = 0;
 
+const rock = document.getElementById("ROCK");
+const paper = document.getElementById("PAPER");
+const scissors = document.getElementById("SCISSORS");
+const reset = document.getElementById("RESET");
+
+rock.addEventListener("click", () => {playRound(rock.id); }, false);
+paper.addEventListener("click", () => {playRound(paper.id); }, false);
+scissors.addEventListener("click", () => {playRound(scissors.id); }, false);
+reset.addEventListener("click", function() {
+    document.location.reload();
+});
+
+const score = document.querySelector("#SCORE");
+const score2 = document.querySelector("#SCORE2");
+const currentround = document.querySelector("#CURRENTROUND");
+const final = document.querySelector("#FINAL")
+
 //This function selects a random rock, paper, or scissors for the computer.
 function computerPlay() {
     var computerrandom = Math.floor((Math.random() * 3) + 1)
@@ -15,66 +32,51 @@ function computerPlay() {
 
 
 //This function plays a round and updates the score accordingly.
-function playRound(player, computerSelection) {
+function playRound(player) {
+    var computerSelection = computerPlay();
     if (player === computerSelection) {
-        return("You have tied!")
+        currentround.textContent = "You have tied!"
     } else if (player === "ROCK") {
         if  (computerSelection === "PAPER") {
             computerscore += 1
-            return("You lose! Paper beats rock!")
+            currentround.textContent = "You lose! Paper beats rock!"
         } if (computerSelection === "SCISSORS") {
             playerscore += 1
-            return("You win!  Rock beats scissors!")
+            currentround.textContent = "You win!  Rock beats scissors!"
         }
     } else if (player === "PAPER") {
         if (computerSelection === "ROCK") {
             playerscore += 1
-            return("You win!  Paper beats rock!")
+            currentround.textContent = "You win!  Paper beats rock!"
         } if (computerSelection === "SCISSORS") {
             computerscore += 1
-            return("You lose!  Scissors beats paper!")
+            currentround.textContent = "You lose!  Scissors beats paper!"
         }
     } else {
         if (computerSelection === "ROCK") {
             computerscore += 1
-            return("You lose!  Rock beats scissors!")
+            currentround.textContent = "You lose!  Rock beats scissors!"
         } if (computerSelection === "PAPER") {
             playerscore += 1
-            return("You win!  Scissors beats paper!")
+            currentround.textContent = "You win!  Scissors beats paper!"
         }
-    } 
-}
-
-//This function takes player input and checks it is valid.
-function playerChoice() {
-    var player = prompt("Rock, paper, or scissors?").toUpperCase()
-    if (player == "ROCK" || player == "PAPER" || player == "SCISSORS") {
-        return(player);       
-    } else {
-        console.log("You must choose only rock, paper, or scissors!")
-        return(playerChoice());
     }
-}
+    score.textContent = "You: " + playerscore;
+    score2.textContent = "Computer: " + computerscore; 
 
-//This function plays a game until someone has 5 points, and then outputs the winner.
-function game() {
-    if (playerscore != 5 && computerscore != 5) {
-        var computerSelection = computerPlay()
-        var player = playerChoice()
-        console.log(playRound(player, computerSelection))
-        return(game())
-    } else {
+    if (playerscore >= 5 || computerscore >= 5) {
+        rock.disabled = true;
+        paper.disabled = true;
+        scissors.disabled = true;
         if (playerscore > computerscore) {
-            console.log("You win!  Final score is " + playerscore + " to " + computerscore + "!")
-            return 0;
+            final.textContent = "You win!  Final score is " + playerscore + " to " + computerscore + "!"
         } else if (computerscore > playerscore) {
-            console.log("You lose!  Final score is " + playerscore + " to " + computerscore + "!")
-            return 0;
+            final.textContent = "You lose!  Final score is " + playerscore + " to " + computerscore + "!"
         } else {
-            console.log("You tied! Final score is " + playerscore + " to " + computerscore + "!")
-            return 0;
+            final.textContent = "You tied! Final score is " + playerscore + " to " + computerscore + "!"
+            }
         }
-    }
+
 }
 
-game()
+
